0bb0187e67d8f31597be3d6b7cac6f538d3927c9
UttamSaha <uttamsaha982@gmail.com>
2022-02-26 16:53:32 +0600

    region added

--- js/app.js
+++ js/app.js
@@ -25,10 +25,34 @@ const displayCountires = countries => {
 loadCountries();
 
 const all = document.getElementById('all').addEventListener('click', function(){
-    // location.reload();
+    location.reload();
 })
 
-//load asian
-const loadAsian = () => {
-    git 
+document.getElementById('region').addEventListener('click', function(event){
+    console.log();
+    const region = event.target.innerText;
+    // console.log(id.innerText);
+    fetch(`https://restcountries.com/v3.1/region/${region}`)
+    .then(res => res.json())
+    .then(data => displayCountry(data))
+})
+const displayCountry = countries => {
+    console.log(countries);
+    const divContainer = document.getElementById('country');
+    divContainer.innerHTML = '';
+    countries.forEach(country => {
+        const div = document.createElement('div');
+        div.className = "country";
+        div.innerHTML = `
+        <img width="100px" src="${country.flags.png}"
+        <span>${country.name.common}</span>
+        <p><span>Capital: </span>${country.capital}</p>
+        <p><span>Population: </span>${country.population}</p>
+        <p><span>Region: </span>${country.region}</p>
+        <p><span>Time zone: </span>${country.timezones}</p>
+        `
+        divContainer.appendChild(div);
+        // console.log(country);
+    })
+
 }
